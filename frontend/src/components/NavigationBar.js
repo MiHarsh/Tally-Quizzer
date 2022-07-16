9d2e312a9e0bc5448015379bd21952666fbeb509
divyanshu887 <singhdivyanshu364@gmail.com>
2022-07-16 10:50:06 +0530

    added quiz list ui

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -27,6 +27,7 @@ export default function NavigationBar() {
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/createquiz">Create Quiz</Nav.Link>
           <Nav.Link href="/studentLogin">StudentLogin</Nav.Link>
+          <Nav.Link href="quizes">Quizes</Nav.Link>
           {currentUser && (
             <>
               <Nav.Link href="/dashboard">Dashboard</Nav.Link>
